4a1de5f10b64410c9a511d7d599b4757bb380ee4
kuronekomone <kuroneko.mone@docomo.ne.jp>
2017-02-15 16:26:27 +0900

    Add files via upload

--- DBManager.java
+++ DBManager.java
@@ -17,11 +17,13 @@ public class DBManager{
 		try{
 			
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
 			String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-			Connection con = DriverManager.getConnection(url,"sys", "orcl");
+			Connection con = DriverManager.getConnection(url,"os", "wiki");
+			
 			
 			return con;
-		
+			
 		}catch(Exception e){
 			throw new IllegalStateException(e);
 		}
